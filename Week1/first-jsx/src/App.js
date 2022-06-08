03a4e2e12bdaa6e1b25e4425a34ea6d68866685c
John Doe <johndoe@example.com>
2022-06-08 11:41:37 -0700

    push new changes

---
+++ Week1/first-jsx/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do!</h3>
+      <ul>
+        <li>Learn React</li>
+        <li>Be better at Javascript</li>
+        <li>Travel the World!</li>
+        <li>Never stop learning code...</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
